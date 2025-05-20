0bf373ada3f3ca5b6e8fed60f6869c352874f3da
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-05-20 08:25:35 -0500

    Tkinter window created

---
+++ main.py
@@ -0,0 +1,5 @@
+from window import Window
+
+
+win = Window(1000, 800)
+win.wait_for_close()
---
+++ window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.title("Maze Py solver")
+        self.canvas = Canvas(self.__root, width=self.width, height=self.height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.is_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
