64e954baaefc537293645f5b90b5e48cfc59beae
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-05-20 20:56:01 -0500

    Lines added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from figures import Line, Point
 from window import Window
 
-
 win = Window(1000, 800)
+new_line = Line(Point(30, 50), Point(100, 50))
+win.draw_line(new_line, "black")
+new_line2 = Line(Point(30, 50), Point(1000, 70))
+win.draw_line(new_line2, "black")
 win.wait_for_close()
+
--- window.py
+++ window.py
@@ -14,6 +14,7 @@ class Window():
 
     def redraw(self):
         self.__root.update_idletasks()
+        self.draw_line
         self.__root.update()
 
     def wait_for_close(self):
@@ -23,4 +24,7 @@ class Window():
 
     def close(self):
         self.is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
